5b780d0b31a5e93afa24f4f40a95203ba0cef1d0
gkovan <gkovan@hotmail.com>
2022-06-09 23:33:59 -0400

    basic scaffolding working

--- src/main/java/application/app/Application.java
+++ src/main/java/application/app/Application.java
@@ -14,7 +14,7 @@ import org.springframework.core.env.Environment;
 
 @ServletComponentScan({"com.ibm.orderskafka.*", "com.ibm.kafkastream.wordcount.*", "com.ibm.kafkastream.pipe.*", "com.ibm.kafkastream.mapping.*", "com.ibm.kafkastream.window.*", "com.ibm.kafkastream.sessionwindow.*", "com.ibm.kafkastream.json.*"})
 @SpringBootApplication
-@ComponentScan({"com.ibm.hello.*","com.ibm.simplekafka.*", "com.ibm.orderskafka.*", "com.ibm.config", "com.ibm.kafkastream.wordcount.*", "com.ibm.kafkastream.pipe.*", "com.ibm.kafkastream.mapping.*", "com.ibm.kafkastream.window.*", "com.ibm.kafkastream.json.*", "com.ibm.cloud_garage.*", "com.ibm.health"})
+@ComponentScan({"com.ibm.hello.*","com.ibm.simplekafka.*", "com.ibm.orderskafka.*", "com.ibm.config", "com.ibm.kafkastream.wordcount.*", "com.ibm.kafkastream.pipe.*", "com.ibm.kafkastream.mapping.*", "com.ibm.kafkastream.window.*", "com.ibm.kafkastream.sessionwindow.*", "com.ibm.kafkastream.json.*", "com.ibm.cloud_garage.*", "com.ibm.health"})
 public class Application extends SpringBootServletInitializer {
     @Autowired
     Environment environment;
